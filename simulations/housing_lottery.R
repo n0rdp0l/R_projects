afa384bcd98a7c0acf6659b0c3fcf55f1dced08f
n0rdp0l <63914035+n0rdp0l@users.noreply.github.com>
2023-06-08 11:20:54 +0200

    updated housing lottery file

--- simulations/housing_lottery.R
+++ simulations/housing_lottery.R
@@ -1,24 +1,39 @@
 
 calculate_probability <- function(weeks, overall_prob) {
-  # Calculate the minimum weekly probability of winning
+  # Calculate the minimum weekly probability of winning (Geometric CDF)
   weekly_prob_win <- 1 - (1 - overall_prob)^(1/weeks)
   
-  return(weekly_prob_win)
+  # Calculate alongside the number of contestants that equate this probability 
+  # (2/x = prop)
+  contestants <- (1/weekly_prob_win) * 2
+  
+  return(c(weekly_prob_win,contestants))
 }
+
+
 
 weeks <- 12
 
 # Generate a sequence of overall probabilities
 overall_probs <- seq(0, 1, by = 0.01)
 
-# Calculate the corresponding weekly probabilities
+
+# Calculate the corresponding weekly probabilities and contestants using the modified function
 weekly_probs <- sapply(overall_probs, calculate_probability, weeks = weeks)
 
-
 # Plot the results
-plot(weekly_probs, overall_probs, type = 'l', ylab = 'Overall Probability of Winning', xlab = 'Weekly Probability of Winning', main = paste('Relationship for', weeks, 'weeks'))
+plot(weekly_probs[1, ], overall_probs, type = 'l', 
+     ylab = 'Overall Probability of Winning', 
+     xlab = 'Weekly Probability of Winning', 
+     main = paste('Relationship for', weeks, 'weeks'))
 
-weekly_probability(weeks, 0.30)
+plot(weekly_probs[2, ], overall_probs, type = 'l', 
+     ylab = 'Overall Probability of Winning', 
+     xlab = 'Number of (weekly) Contestants',
+     xlim = c(0,200),
+     main = paste('Relationship for', weeks, 'weeks'))
+
+calculate_probability(weeks, 0.50)
 
 
 
